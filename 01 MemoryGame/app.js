d9ec1368860b8e1163e1df6fab28e99a7e09a025
Rohan Singh <rohannsingh98765@gmail.com>
2021-10-30 23:53:15 +0530

    adding js

---
+++ 01 MemoryGame/app.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // card options
+    const cardArray = [
+    {    
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {    
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    
+    {    
+        name: 'cheeseburgers',
+        img: 'images/cheeseburger.png'
+    },
+    {    
+        name: 'cheeseburgers',
+        img: 'images/cheeseburger.png'
+    },
+    
+    {    
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {    
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+
+    {    
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {    
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+
+    {    
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {    
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+
+    {    
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {    
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+
+    {    
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {    
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    ]
+
+
+    const grid = document.querySelector('.grid')
+
+// create your board
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++){
+        var card = document.createElement('img')
+    }
+}
+
+
+
+
+})
